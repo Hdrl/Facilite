6fe2912d923b14ef3cccff9018af962912a08262
Fernando Trindade <fernando.trindade1301@gmail.com>
2025-06-29 19:21:02 -0300

    -painel admin adcionado gerar gelatorio xlsx

--- viagens/models.py
+++ viagens/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from datetime import datetime
 import locale
+
+TIPO_CHOICES = [
+    ('E', 'Entrada'),
+    ('S', 'Saida'),
+]
+
 # Create your models here.
 class Viagen(models.Model):
-    destino=models.CharField(max_length=20)  
+    destino=models.CharField(max_length=20) 
+    empresa=models.CharField(max_length=50, default = "Smart Picking Soluções Digitais Ltda")
+    setor=models.CharField(max_length=10, default="Smart")
+    colaborador=models.CharField(max_length=50, default="Fernando Pereira da Trindade")
+    retorno=models.DateField(blank = True, null = True)
+    motivo=models.CharField(max_length=50)
+    saida=models.CharField(max_length=20, default="Apucarana") 
     
     def __str__(self):
-        return f"{self.destino}"
+        return f"{self.destino} - {self.motivo}"
     
 class Despesa(models.Model):
     valor = models.DecimalField(max_digits=10, decimal_places=2, blank = True, null = True)
@@ -15,6 +27,7 @@ class Despesa(models.Model):
     nota_fiscal = models.URLField(max_length=200, blank = True, null = True)
     imagem = models.ImageField(upload_to='despesas/')
     viagem = models.ForeignKey(Viagen, on_delete=models.CASCADE, related_name='despesas')
+    tipo = models.CharField(max_length=1, choices=TIPO_CHOICES, default='S')
     
     def __str__(self):
         locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
